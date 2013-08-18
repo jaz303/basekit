ee73d7abf84809eff500b09b0f3033e454c4f8d7
Jason Frame <jason@onehackoranother.com>
2013-08-18 09:40:38 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var Class   = require('classkit').Class,
+    signal  = require('signalkit').signal;
+
+var Base = Class.extend(function(sc, sm) {
+
+    return [
+        function() {
+            this._teardown = [];
+        },
+        'methods', {
+            teardown: function() {
+                this._teardown.forEach(function(td) { td(); });
+            },
+
+            _addSignal: function(name) {
+                var sig = signal(name);
+                this[name] = sig;
+                this._teardown.push(function() {
+                    sig.clear();
+                });
+            },
+
+            _bind: function(el, event, handler) {
+                el.addEventListener(event, handler);
+                this._teardown.push(function() {
+                    el.removeEventListener(event, handler);
+                });
+            }
+        }
+    ];
+
+});
